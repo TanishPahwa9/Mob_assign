074fd9d218c44195bc781029b6ea08faa9a05bcc
TanishPahwa9 <tanishpahwa36@gmail.com>
2024-11-16 04:43:42 +0530

    Importing the needfull packages and making the class Transaction and defining data variables.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+class Transaction {
+    String transactionDate;
+    String productCode;
+    double unitPrice;
+    int unitsSold;
+
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
     }
 }
